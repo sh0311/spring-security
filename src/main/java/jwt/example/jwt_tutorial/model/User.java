e6f9ad17dbb86834bba99ac6bdbc4883af46264b
sh0311 <0311alice@naver.com>
2024-07-31 17:54:51 +0900

    섹션 0

---
+++ src/main/java/jwt/example/jwt_tutorial/model/User.java
@@ -0,0 +1,23 @@
+package jwt.example.jwt_tutorial.model;
+import java.sql.Timestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.Data;
+@Data
+@Entity
+public class User {
+    @Id // primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private String role; //ROLE_USER, ROLE_ADMIN
+    @CreationTimestamp
+    private Timestamp createDate;
+}
